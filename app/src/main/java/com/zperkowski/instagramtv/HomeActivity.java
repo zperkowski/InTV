166ae64d9baaf25f998f5b5768ece15b43d1de18
zperkowski <ZdzislawPerkowski@outlook.com>
2017-10-14 16:15:50 +0200

    Created an empty project and gitignore

---
+++ app/src/main/java/com/zperkowski/instagramtv/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.zperkowski.instagramtv;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zperkowski on 14/10/2017.
+ */
+
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_home);
+    }
+
+}
